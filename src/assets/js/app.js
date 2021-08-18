d6b2321318338e748751faa17939ead13f303fcc
byteOfMonkey <byteofmonkey@gmail.com>
2021-08-19 01:27:44 +0300

    gulp template & elements & fonts & intro section

---
+++ src/assets/js/app.js
@@ -0,0 +1,27 @@
+// import Glide from '@glidejs/glide';
+
+
+// import 'jquery/dist/jquery';
+
+// import '@popperjs/core';
+
+// import 'bootstrap/js/dist/util';
+// import 'bootstrap/js/dist/alert';
+// import 'bootstrap/js/dist/button';
+// import 'bootstrap/js/dist/carousel';
+// import 'bootstrap/js/dist/collapse';
+// // import 'bootstrap/js/dist/dropdown';/////////////////////////////////
+// import 'bootstrap/js/dist/modal';
+// // import 'bootstrap/js/dist/tooltip';/////////////////////////////////
+// // import 'bootstrap/js/dist/popover';/////////////////////////////////
+// import 'bootstrap/js/dist/scrollspy';
+// import 'bootstrap/js/dist/tab';
+// import 'bootstrap/js/dist/toast';
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  // ----------------------------------------------------------- slider:
+
+
+})
