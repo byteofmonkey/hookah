6d3bae87b7358fe8dc3132b1275d68f1a9ef1614
byteOfMonkey <byteofmonkey@gmail.com>
2021-08-21 17:31:43 +0300

    slider for reviews section & refactoring

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,8 +1,6 @@
-// import Glide from '@glidejs/glide';
-
+import Glide from '@glidejs/glide';
 
 // import 'jquery/dist/jquery';
-
 // import '@popperjs/core';
 
 // import 'bootstrap/js/dist/util';
@@ -19,9 +17,25 @@
 // import 'bootstrap/js/dist/toast';
 
 
-
 document.addEventListener('DOMContentLoaded', () => {
   // ----------------------------------------------------------- slider:
+  const options = {
+    type: 'carousel',
+    startAt: 0,
+    perView: 3,
+    dragThreshold: false,
+    gap: 30,
+    autoplay: false,
+    keyboard: false,
+    animationDuration: 400,
+    animationTimingFunc: "ease-in-out",
+    breakpoints: {
+      1200: {
+        perView: 1,
+      }
+    }
+  };
 
+  new Glide('.glide', options).mount();
 
 })
